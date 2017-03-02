f2e8189da5adf97d1b4ec1195c3aba2998201196
Mick Tarsel <mtarsel@gmail.com>
2017-03-01 19:11:43 -0800

    first commit

---
+++ cleanup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Mick Tarsel
+#remove bridge, namespace, and bridge interfaces
+
+#NOTE - this doenst grep for anything. i just copied declarations from setup.sh
+#YOUR NAMES COULD BE DIFFERENT
+BRIDGE=br01
+NS1=ns1-client
+NS2=ns2-server
+NS1PORT=br-tap1
+NS2PORT=br-tap2
+
+ip netns delete $NS1
+ip netns delete $NS2
+ip link delete $NS1PORT
+ip link delete $NS2PORT
+ifconfig $BRIDGE down
+brctl delbr $BRIDGE
+
+echo "all gone!!"
+sleep 2
+
+ifconfig
