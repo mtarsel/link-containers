5df45c0b6a5811f964226b2a934261ff74420d22
Mick Tarsel <mtarsel@gmail.com>
2017-03-22 09:50:37 -0700

    added ping count instead of ctrl+c
    Signed-off-by: Mick Tarsel <mtarsel@gmail.com>

--- ovs-testsuite/vlan-isolation.sh
+++ ovs-testsuite/vlan-isolation.sh
@@ -8,18 +8,14 @@ ovs-vsctl add-port br-01 br-p3 tag=100
 ovs-vsctl add-port br-01 br-p2 tag=200
 ovs-vsctl add-port br-01 br-p4 tag=200
 
-echo""
-echo "=============== Hit Ctrl+C ============="
-echo ""
-
 #working pings
-ip netns exec ns1 ping 10.0.0.30
-ip netns exec ns2 ping 10.0.0.40
+ip netns exec ns1 ping -c2 10.0.0.30
+ip netns exec ns2 ping -c2 10.0.0.40
 
 echo ""
 echo "***** Below ping tests should fail. wait for it ******"
-sleep 5
+sleep 2
 
 #this one should not work
-ip netns exec ns2 ping 10.0.0.30
-ip netns exec ns1 ping 10.0.0.40
+ip netns exec ns2 ping -c1 10.0.0.30
+ip netns exec ns1 ping -c1 10.0.0.40
