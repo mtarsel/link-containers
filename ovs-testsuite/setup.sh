9509d1ce6d693947c4f065cc05a2fc09c8219e92
Mick Tarsel <mtarsel@gmail.com>
2017-03-22 20:56:42 -0700

    Creates name spaces. variables are static
    Signed-off-by: Mick Tarsel <mtarsel@gmail.com>

--- ovs-testsuite/setup.sh
+++ ovs-testsuite/setup.sh
@@ -6,29 +6,36 @@
 
 # create the bridge, namespaces, and bridge ports for namespaces
 NUMBRIDGE=1
-NUMNS=2
+NUMNS=4
+NETWORKADD=10.0.0.1
 
 ######### Create OVS switch #########
 for i in `seq 1 $NUMBRIDGE`;
 do
 	ovs-vsctl add-br br-$i
-	ip link set dev br-$i up
-	ifconfig br-$i 10.0.0.1/24 up
+
+#	ip link set dev br-$i up
+#	ifconfig br-$i 10.0.0.1/24 up
 done
 
+#./setup-ns.sh $NUMBRIDGE
+
+exit 1
 
 for i in `seq 1 $NUMNS`;
 do
 	ip netns add ns$i
-
-done	
+	
 	# create a port pair. the tap$i exists in the ns, br-p$i on host 
-#	ip link add tap$i type veth peer name br-p$i
-#	ip netns exec ns$i ip link set dev tap$i up
-#	ip link set dev br-p$i up 
+	ip link add tap$i type veth peer name p$i
+	ip link set tap$i netns ns$i
+	ip netns exec ns$i ip link set dev tap$i up
+	ip link set dev p$i up 
+
 	#assign ip addresses.
-#	ifconfig br-p$ up
-#	ip netns exec ns$i ifconfig tap$i 10.0.0.$i0/24 up
+	ifconfig p$ up
+	ip netns exec ns$i ifconfig tap$i 10.0.0.'$i'0/24 up
+done
 
 echo "###### List of Namespaces #####"
 ip netns list
---
+++ ovs-testsuite/setup/ns.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+BR=1
+#NUMBRIDGE=$1
+NUMNS=4
+IP=10.0.0.0
+
+for i in `seq 1 $NUMNS`;
+do
+        ip netns add ns$i
+#	ovs-vsctl add-port br-$NUMBRIDGE p$i
+
+        # create a port pair. the tap$i exists in the ns, br-p$i on host 
+        ip link add tap$i type veth peer name p$i
+        ip link set tap$i netns ns$i
+        ip netns exec ns$i ip link set dev tap$i up
+        ip link set dev p$i up
+	ovs-vsctl add-port br-$BR p$i
+        
+        #assign ip addresses.
+       # ifconfig p$ up
+	#add ten to last octet of network ip addr
+	#echo $IP
+	#IP=$(echo $IP | awk -F. '{printf "%d.%d.%d.%d", $1,$2,$3,$4+10}')
+	#echo $IP
+#        ip netns exec ns$i ifconfig tap$i 10.0.0.'$i'0/24 up
+done
+
