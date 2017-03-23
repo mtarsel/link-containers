8ffd406d47a8c9ccd14f214d7c7b29591173fa18
Mick Tarsel <mtarsel@gmail.com>
2017-03-22 21:24:54 -0700

    moved better setup scripts into setup/
    Signed-off-by: Mick Tarsel <mtarsel@gmail.com>

--- ovs-testsuite/setup/dumb-bridge.sh
+++ ovs-testsuite/setup/dumb-bridge.sh
@@ -5,7 +5,7 @@ ovs-vsctl add-br br0 -- set Bridge br0 fail-mode=secure
 
 #setup
 for i in 1 2 3 4; do
-    ovs-vsctl add-port br0 p$i
+    ovs-vsctl add-port br0 p-$i
     set Interface p$i ofport_request=$i
    # ovs-ofctl mod-port br0 p$i up
 done
