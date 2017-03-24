210a1e80157a4ab02e7260da39d81986a29af351
Mick Tarsel <mtarsel@gmail.com>
2017-03-24 00:24:49 -0700

    moved to ip commands only. works and commented. including this one in tex plan
    Signed-off-by: Mick Tarsel <mtarsel@gmail.com>

--- ovs-testsuite/setup/bridge.sh
+++ ovs-testsuite/setup/bridge.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-
-
-# create the bridge, namespaces, and bridge ports for namespaces
+# create the bridge
 NUMBRIDGE=1
+IP=00.0.0.1
 
-#TODO
-IP=10.0.0.1
-#next_serv=$(echo $NETWORKADD | awk -F. '{printf "%d.%d.%d.%d", $1,$2,$3,$4-1}')
-#outputs: 10.0.0.0
-
-######### Create OVS switch #########
 for i in `seq 1 $NUMBRIDGE`;
 do
+	# Create OVS switch 
         ovs-vsctl add-br br-$i
-	echo $i
-       ip link set dev br-$i up
-	IP=$(echo $IP | awk -F. '{printf "%d.%d.%d.%d", $1,$2+100,$3,$4}')
-	echo $IP
-       ifconfig br-$i $IP/24 up
-done
+	
+	#turn the bridge on
+	ip link set dev br-$i up
+	
+	#adds 100 to second octet in ip for different subnets
+	IP=$(echo $IP | awk -F. '{printf "%d.%d.%d.%d", $1+10,$2,$3,$4}')
 
+	#assign IP address
+	ip addr add $IP/24 dev br-$i
+	
+	#bring the bridge interface up
+	ip link set br-$i up
+done
 
 ovs-vsctl show
