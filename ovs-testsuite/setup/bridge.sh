c7215dab11671068dfc0a6bbb531d95c789361f1
Mick Tarsel <mtarsel@gmail.com>
2017-03-24 00:14:41 -0700

    added for loop for making many bridges
    Signed-off-by: Mick Tarsel <mtarsel@gmail.com>

--- ovs-testsuite/setup/bridge.sh
+++ ovs-testsuite/setup/bridge.sh
@@ -2,10 +2,10 @@
 
 
 # create the bridge, namespaces, and bridge ports for namespaces
-NUMBRIDGE=1
+NUMBRIDGE=2
 
 #TODO
-#NETWORKADD=10.0.0.1
+IP=10.0.0.1
 #next_serv=$(echo $NETWORKADD | awk -F. '{printf "%d.%d.%d.%d", $1,$2,$3,$4-1}')
 #outputs: 10.0.0.0
 
@@ -13,9 +13,12 @@ NUMBRIDGE=1
 for i in `seq 1 $NUMBRIDGE`;
 do
         ovs-vsctl add-br br-$i
-
+	echo $i
        ip link set dev br-$i up
-       ifconfig br-$i 10.0.0.1/24 up
+	IP=$(echo $IP | awk -F. '{printf "%d.%d.%d.%d", $1,$2+100,$3,$4}')
+	echo $IP
+       ifconfig br-$i $IP/24 up
 done
+
 
 ovs-vsctl show
