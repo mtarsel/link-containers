fbf01da932dc22400fa07ead93c3c07b990ab69b
Mick Tarsel <mtarsel@gmail.com>
2017-04-12 20:26:52 -0700

    Creates a bridge on 10.0.0.1/8 network
    Signed-off-by: Mick Tarsel <mtarsel@gmail.com>

--- ovs-testsuite/setup/bridge.sh
+++ ovs-testsuite/setup/bridge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # create the bridge
-NUMBRIDGE=1
+NUMBRIDGE=$1
 IP=00.0.0.1
 
 for i in `seq 1 $NUMBRIDGE`;
@@ -11,11 +11,12 @@ do
 	#turn the bridge on
 	ip link set dev br-$i up
 	
-	#adds 100 to second octet in ip for different subnets
+	#adds 10 to first octet in ip for different subnets
 	IP=$(echo $IP | awk -F. '{printf "%d.%d.%d.%d", $1+10,$2,$3,$4}')
-
+		
 	#assign IP address
-	ip addr add $IP/24 dev br-$i
+	#ip addr add $IP/24 dev br-$i
+	ip addr add $IP/8 dev br-$i
 	
 	#bring the bridge interface up
 	ip link set br-$i up
